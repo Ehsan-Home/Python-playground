31bcd35e392c6244897948177f9b7547839384c1
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-06-10 15:37:03 -0700

    working with encryptions

--- src.py
+++ src.py
@@ -1,17 +1,20 @@
-dec = {
-    "address": {
-        "public-key":"PK",
-        "private-key":"SK"
-    }
-}
-
-# publicKey = dec["address"]["public-key"]
-# print(publicKey)
-
-address2 = {
-    "public-key":"PK2",
-    "private-key":"SK2"
-}
-dec["address2"] = address2
-
-print(dec)
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import binascii
+ 
+keyPair = RSA.generate(3072)
+ 
+pubKey = keyPair.publickey()
+print(f"Public key:  (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
+pubKeyPEM = pubKey.exportKey()
+print(pubKeyPEM.decode('ascii'))
+ 
+print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
+privKeyPEM = keyPair.exportKey()
+print(privKeyPEM.decode('ascii'))
+ 
+#encryption
+msg = 'A message for encryption'
+encryptor = PKCS1_OAEP.new(pubKey)
+encrypted = encryptor.encrypt(msg.encode())
+print("Encrypted:", binascii.hexlify(encrypted))
