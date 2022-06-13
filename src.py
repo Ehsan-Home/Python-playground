46cfaa42ca17a2261b3573c20e4802c1d6ae033b
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-06-13 11:43:20 -0700

    working with \ in strings

--- src.py
+++ src.py
@@ -1,20 +1,13 @@
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
-import binascii
- 
-keyPair = RSA.generate(3072)
- 
-pubKey = keyPair.publickey()
-print(f"Public key:  (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
-pubKeyPEM = pubKey.exportKey()
-print(pubKeyPEM.decode('ascii'))
- 
-print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
-privKeyPEM = keyPair.exportKey()
-print(privKeyPEM.decode('ascii'))
- 
-#encryption
-msg = 'A message for encryption'
-encryptor = PKCS1_OAEP.new(pubKey)
-encrypted = encryptor.encrypt(msg.encode())
-print("Encrypted:", binascii.hexlify(encrypted))
+# str = "ehsan\\n\m"
+
+# str = input()
+# # str = "\n\n\m"
+
+# dict = {"name":str.encode()}
+
+# str2 = dict["name"]
+
+# # print(len(str))
+# print(str)
+# print(str2)
+
